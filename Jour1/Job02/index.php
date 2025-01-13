a9387a970ac10198759279682a1f9590a064d3f6
Sarah Hamitouche <sarah.hamitouche@laplateforme.io>
2025-01-13 16:31:01 +0100

    Finis

---
+++ Jour1/Job02/index.php
@@ -0,0 +1,10 @@
+<?php
+// Afficher "Hello LaPlateforme!" (commentaire sur une ligne)
+echo "Hello LaPlateforme!"; 
+
+/*
+  Afficher "Hello LaPlateforme!" une deuxième fois
+  (commentaire sur un bloc)
+*/
+echo "Hello LaPlateforme!";
+?>
