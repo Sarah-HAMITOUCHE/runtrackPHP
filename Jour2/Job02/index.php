2382e8b5b68b47ef4d9f539d69650428b9cdaa8c
Sarah Hamitouche <sarah.hamitouche@laplateforme.io>
2025-01-14 14:35:51 +0100

    Add index.php to display numbers while skipping specific values

---
+++ Jour2/Job02/index.php
@@ -0,0 +1,14 @@
+
+<?php
+    for ($i = 0; $i <= 1337; $i++) {
+            // Vérifie si le nombre est 26, 37, 88 ou 1111
+             // if ($i == 26 && $i == 37 && $i == 88 && $i == 1111) 
+     if ($i == 26 || $i == 37 || $i == 88 || $i == 1111) {
+        continue;  // Ignore ces nombres
+            }
+            // Affiche les autres nombres avec un retour à la ligne
+        echo "$i<br>";
+        }
+        ?>
+        
+
